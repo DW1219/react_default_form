75a0cbed8d3683ee87ef4ff05ba1aff087814535
dw1219 <ymg886@naver.com>
2021-08-02 16:14:40 +0900

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+//import './App.css';
+//import MainPage from "./views/MainPage/MainPage.js";
+import { Route, Switch } from "react-router-dom";
+import MainPage from './views/MainPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Route exact path="/" component={MainPage} />
+    </div>
+  );
+}
+
+export default App;
