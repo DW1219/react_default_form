a278bf6b0dae64b519619748aef94aa62a1cc669
DW1219 <ymg886@naver.com>
2022-10-11 18:08:13 +0900

    tot clean version

--- src/components/message/MessageForm.js
+++ src/components/message/MessageForm.js
@@ -1,22 +1,21 @@
-//import { format } from "timeago.js";
-import "./messageForm.css";
+import './messageForm.css';
 
 function MessageForm({ message, own, icon }) {
   return (
-    <div className={own ? "message own" : "message"}>
+    <div className={own ? 'message own' : 'message'}>
       <div className="messageTop">
         <img
           className="messageImg"
           src={
             icon !== null
               ? icon
-              : "https://www.businesspost.co.kr/news/photo/202203/20220310055124_30395.jpg"
+              : //: 'https://www.businesspost.co.kr/news/photo/202203/20220310055124_30395.jpg'
+                'https://images.chosun.com/resizer/7fXeUbYGUI9_MWDKxDvD3PBUM3c=/210x210/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/TLAG2JUEF5DVJEQFYXCDAFW4FA.png'
           }
           alt=""
         />
         <p className="messageText">{message}</p>
       </div>
-      {/*<div className="messageBottom">{format(message.createdAt)}</div>*/}
     </div>
   );
 }
--- src/components/views/MainPage.js
+++ src/components/views/MainPage.js
@@ -1,123 +1,60 @@
 import { useMqttState, useSubscription } from 'mqtt-react-hooks';
 import React, { useEffect, useRef, useState } from 'react';
-//import ChatOnline from "../../components/chatOnline/ChatOnline";
+import Tobbar from '../../components/topbar/Topbar';
 import MessageForm from '../message/MessageForm';
 import './messenger.css';
-//import { AuthContext } from "../../context/AuthContext";
-import Tobbar from '../../components/topbar/Topbar';
 
 const myId = 'biden';
 
+//http://test3.bonkab.com/?topic={채널명}&id={내아이디}
+
 function MainPage() {
-  const { connectionStatus } = useMqttState();
+  const { client } = useMqttState();
   const { message } = useSubscription(['test', 'fuck']);
 
-  const [conversations, setConversations] = useState([]);
-  const [currentChat, setCurrentChat] = useState(true);
   const [messages, setMessages] = useState([
     { text: '와 손흥민 너무 잘한다~!! 최고!!', id: 'biden' },
   ]);
   const [newMessage, setNewMessage] = useState('');
-  //const [arrivalMessage, setArrivalMessage] = useState(null);
-  //const [onlineUsers, setOnlineUsers] = useState([]);
-  //const socket = useRef();
-  //const { user } = useContext(AuthContext);
+
   const scrollRef = useRef();
 
-  /*
   useEffect(() => {
-    socket.current = io("ws://localhost:8900");
-    socket.current.on("getMessage", (data) => {
-      setArrivalMessage({
-        sender: data.senderId,
-        text: data.text,
-        createdAt: Date.now(),
-      });
-    });
-  }, []);
-*/
-  /*
-  useEffect(() => {
-    arrivalMessage &&
-      currentChat?.members.includes(arrivalMessage.sender) &&
-      setMessages((prev) => [...prev, arrivalMessage]);
-  }, [arrivalMessage, currentChat]);
-*/
+    console.log('new message arrived : ', message);
 
-  useEffect(() => {
-    setMessages((prev) => [...prev, { text: message?.message, id: myId }]);
+    if (message) {
+      const { topic: topicValue, message: messageValue } = message;
+      console.log('parsed messageValue : ', messageValue);
+      try {
+        let parsedMsg = JSON.parse(messageValue);
+        //let receiveData = message?.text;
+        console.log('parded parsedMsg.text: ', parsedMsg.text);
+        console.log('parded parsedId.id: ', parsedMsg.id);
+        console.log('topic value : ', topicValue);
+
+        setMessages((prev) => [
+          ...prev,
+          { text: parsedMsg.text, id: parsedMsg.id },
+        ]);
+      } catch (err) {
+        console.log('errrrr');
+        setMessages((prev) => [
+          ...prev,
+          { text: message?.message, id: 'trump' },
+        ]);
+      }
+    }
   }, [message]);
 
-  /*
-  useEffect(() => {
-    socket.current.emit("addUser", user._id);
-    socket.current.on("getUsers", (users) => {
-      setOnlineUsers(
-        user.followings.filter((f) => users.some((u) => u.userId === f))
-      );
-    });
-  }, [user]);
-*/
-  /*
-  useEffect(() => {
-    const getConversations = async () => {
-      try {
-        const res = await axios.get("/conversations/" + user._id);
-        setConversations(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getConversations();
-  }, [user._id]);
-*/
-  /*
-  useEffect(() => {
-    const getMessages = async () => {
-      try {
-        const res = await axios.get("/messages/" + currentChat?._id);
-        setMessages(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getMessages();
-  }, [currentChat]);
-*/
-
   const handleSubmit = () => {
-    setMessages((prev) => [...prev, { text: newMessage, id: myId }]);
-  };
-
-  /*
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const message = {
-      sender: user._id,
+    const jsonFormat = {
+      id: myId,
       text: newMessage,
-      conversationId: currentChat._id,
     };
-
-    const receiverId = currentChat.members.find(
-      (member) => member !== user._id
-    );
-
-    socket.current.emit("sendMessage", {
-      senderId: user._id,
-      receiverId,
-      text: newMessage,
-    });
-
-    try {
-      const res = await axios.post("/messages", message);
-      setMessages([...messages, res.data]);
-      setNewMessage("");
-    } catch (err) {
-      console.log(err);
-    }
-  };*/
-
-  useEffect(() => {}, []);
+    setMessages((prev) => [...prev, { text: newMessage, id: myId }]);
+    return client.publish('test', JSON.stringify(jsonFormat));
+    //return client.publish('test', jsonFormat);
+  };
 
   useEffect(() => {
     scrollRef.current?.scrollIntoView({ behavior: 'smooth' });
@@ -126,103 +63,36 @@ function MainPage() {
   return (
     <>
       <Tobbar />
-      <div>{`Connect Status : ${connectionStatus} | Current Topic : ${message?.topic}`}</div>
+      <div>{`Connect Status : ${client} | Current Topic : ${message?.topic}`}</div>
       <div style={{ display: 'flex', flexDirection: 'column' }}></div>
 
       <div className="messenger">
-        {/*<div className="chatMenu">
-          <div className="chatMenuWrapper">
-            {<input placeholder="Search for friends" className="chatMenuInput" />}
-            {conversations.map((c) => (
-              <div onClick={()=> setCurrentChat(c)}>
-                <Conversation conversation={c} currentUser={user} />
-              </div>
-            ))}
-          </div>
-        </div>*/}
         <div className="chatBox">
           <div className="chatBoxWrapper">
-            {currentChat ? (
-              <>
-                <div className="chatBoxTop">
-                  {/*}
+            <div className="chatBoxTop">
+              {messages.map((m) => (
+                <div ref={scrollRef}>
                   <MessageForm
-                    message="와 손흥민 너무 잘한다~!! 최고!!"
-                    own={true}
-                    icon="https://theyouthdream.com/files/attach/images/2021/11/18/f8571524b4d791beffb683b3557c12f9.gif"
-                  />
-                  <MessageForm message="천상계 급이다" own={false} icon={null} />
-                  <MessageForm
-                    message="이렇게 국봉이 차오른다!!!"
-                    own={false}
-                    icon="https://cdn.imweb.me/upload/S20170119587fc1996d25a/5a540781d256c.gif"
-                  />
-                  <MessageForm
-                    message="프리킥 성공율 쩐다!!!!!!!!!!"
-                    own={true}
-                    icon="https://theyouthdream.com/files/attach/images/2021/11/18/f8571524b4d791beffb683b3557c12f9.gif"
-                  />
-                  <MessageForm
-                    message="대한민국!!!~!!!!!!"
-                    own={false}
+                    message={m.text}
+                    own={myId === m.id}
                     icon={null}
                   />
-                  <MessageForm
-                    message="오 필승 코리아~~~~~~~~~~"
-                    own={false}
-                    icon={null}
-                  />
-                  <MessageForm
-                    message="우루과이 이기자 가즈아~~~~!!!"
-                    own={false}
-                    icon={null}
-                  />
-                  <MessageForm
-                    message="2006년 패배를 갚아주자!!!"
-                    own={false}
-                    icon={null}
-                  />
-                  <MessageForm message="5:0 예상~!!!!!" own={false} icon={null} />{*/}
-
-                  {messages.map((m) => (
-                    <div ref={scrollRef}>
-                      <MessageForm
-                        message={m.text}
-                        own={myId === m.id}
-                        //own={m.owner}
-                        icon={null}
-                      />
-                    </div>
-                  ))}
                 </div>
-                <div className="chatBoxBottom">
-                  <textarea
-                    className="chatMessageInput"
-                    placeholder="Input..."
-                    onChange={(e) => setNewMessage(e.target.value)}
-                    value={newMessage}
-                  ></textarea>
-                  <button className="chatSubmitButton" onClick={handleSubmit}>
-                    Send
-                  </button>
-                </div>
-              </>
-            ) : (
-              <span className="noConversationText">
-                Open a conversation to start a chat.
-              </span>
-            )}
+              ))}
+            </div>
+            <div className="chatBoxBottom">
+              <textarea
+                className="chatMessageInput"
+                placeholder="Input..."
+                onChange={(e) => setNewMessage(e.target.value)}
+                value={newMessage}
+              ></textarea>
+              <button className="chatSubmitButton" onClick={handleSubmit}>
+                Send
+              </button>
+            </div>
           </div>
         </div>
-        {/*<div className="chatOnline">
-          <div className="chatOnlineWrapper">
-                      <ChatOnline
-              onlineUsers={onlineUsers}
-              //currentId={user._id}
-              setCurrentChat={setCurrentChat}
-            />
-          </div>
-        </div>*/}
       </div>
     </>
   );
